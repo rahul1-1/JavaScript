ded1369c78edfbc737a9a6c2e8ab9851bd2ab27f
Rahul Choudhary <rc9782276591@gmail.com>
2023-07-21 08:47:46 +0530

    Currying

--- Currying.js
+++ Currying.js
@@ -11,24 +11,94 @@
 
     // Using Bind method //
 
-let multiplyByTwo = multiply.bind(this,2);
+// let multiplyByTwo = multiply.bind(this,2);
 // let multiplyByTwo = multiply.bind(this,2,4); // 8  x=2 ,y=4
 
-multiplyByTwo(5,9); //  when multiply.bind(this) than retsult 45 otherwise 2*5 = 10;
+// multiplyByTwo(5,9); //  when multiply.bind(this) than retsult 45 otherwise 2*5 = 10;
 
-let multiplyByThree = multiply.bind(this,3);
-multiplyByThree(8) //24
+// let multiplyByThree = multiply.bind(this,3);
+// multiplyByThree(8) //24
 
 
-   // Using Closures method //
+   // -----------------------Using Closures method ---------- //
 
-function multiply(x){
-    return function (y){
-        console.log(x*y);
-    }
-}
+// function multiply(x){
+//     return function (y){
+//         console.log(x*y);
+//     }
+// }
 
-let mulBy2 = multiply(2);
-mulBy2(8) // 16
-let mulBy3 = multiply(3);
-mulBy3(8)// 24
+// let mulBy2 = multiply(2);
+// mulBy2(8) // 16
+// let mulBy3 = multiply(3);
+// mulBy3(8)// 24
+
+
+//**************************************************************** */
+
+// Infinite Currying
+
+    // function add(a){
+    //     return function(b){
+    //         if(b) return add(a+b);
+    //         return a;
+    //     }
+    // }
+
+    // console.log(add(5)(2)(4)(5)(8)());
+
+//**************************************************************** */
+
+    // converts f(a,b,c) into f(a)(b)(c)
+   
+    //    function curry(func){
+        
+    //        return function curriedFunc(...args){
+    //      //console.log(args, " - ",func)
+    //      // func.length =  number of arguments the function expects to receive.
+    //            if(args.length>=func.length){
+    //                return func(...args);
+    //            } else{
+    //            return function (...next){
+    //                return curriedFunc(...args, ...next);
+    //            }
+    //            }
+    //        }
+    //    }
+   
+    //    const sum = (a,b,c,d,e) => a+b+c+d+e;
+   
+    //    const totalSum = curry(sum);
+    //    console.log(totalSum(1)(2)(3)(4)(5))
+
+
+
+
+    //**************************************************************** */
+
+
+// //      Q   
+//           // sum(1)(2)(3)(4)(5) //15
+//           // sum(1,2)(3)(4)(5) //15
+//           // sum(1,2,3)(4)(5) //15
+//           // sum(1,2)(3)(4,5) //15
+//           // sum (1,2,3,4,5)  //15
+
+//           function curry(fn, ...args){
+//             if(args.length >= fn.length){
+//                 return fn(...args)
+//             } else {
+//                 return function (...nextArgs){
+//                     return curry(fn, ...args,...nextArgs);
+//                 };
+//             }
+//           }
+
+//            sumFiveNumber = (a,b,c,d,e) => a+b+c+d+e;
+//            const sum = curry(sumFiveNumber)
+
+//           console.log(sum(1)(2)(3)(4)(5)) //15
+//           console.log(sum(1,2)(3)(4)(5)) //15
+//           console.log(sum(1,2,3)(4)(5) )//15
+//           console.log(sum(1,2)(3)(4,5) )//15
+//           console.log(sum (1,2,3,4,5)  )//15
