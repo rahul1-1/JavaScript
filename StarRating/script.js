ed749322ff852af5f50d8f7ad5dcb9cb8f1935e9
Rahul Choudhary <rc9782276591@gmail.com>
2023-08-15 08:57:33 +0530

    Star Rating Component

---
+++ StarRating/script.js
@@ -0,0 +1,48 @@
+(function (){
+let starContainer = document.querySelector(".star__container");
+let star = document.querySelectorAll(".stars");
+let totalRating = document.querySelector(".total__rating")
+console.log(star)
+
+let preRating = 0;
+starContainer.addEventListener("click" ,function(e){
+   let currRating = e.target.dataset.index
+    for(let i =0 ;i<preRating;i++)
+    {
+        star[i].classList.remove("star-filled")
+    }
+    for(let i =0 ;i<currRating;i++)
+    {
+        star[i].classList.add("star-filled")
+    }
+    preRating = currRating
+    totalRating.innerHTML=`Total Rating : ${preRating}`
+})
+
+starContainer.addEventListener("mouseover" ,function(e){
+   let currRating = e.target.dataset.index
+    for(let i =0 ;i<5;i++)
+    {
+        star[i].classList.remove("star-filled")
+    }
+    for(let i =0 ;i<currRating;i++)
+    {
+        star[i].classList.add("star-filled")
+    }
+    totalRating.innerHTML=`Total Rating : ${preRating}`
+})
+
+starContainer.addEventListener("mouseleave" ,function(e){
+    for(let i =0 ;i<5;i++)
+    {
+        star[i].classList.remove("star-filled")
+    }
+    for(let i =0 ;i<preRating;i++)
+    {
+        star[i].classList.add("star-filled")
+    }
+    totalRating.innerHTML=`Total Rating : ${preRating?preRating:0}`
+})
+
+
+})()
